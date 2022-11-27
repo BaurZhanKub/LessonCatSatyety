1c9d404c17b4fb1e730345797d109c1bea77c9fa
BaurZhanKub <84919873+BaurZhanKub@users.noreply.github.com>
2022-11-27 22:06:44 +0600

    First Commit

--- src/LessenStart.java
+++ src/LessenStart.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class LessenStart {
     static Scanner sn = new Scanner(System.in);
     public static void main(String[] args) {
+        int catSatyety = 0;
 
+        System.out.println("Введите название еды из списка");
+        //
+        String input = sn.nextLine().toUpperCase();
+        Food food = Food.valueOf(input);
+        catSatyety = catSatyety + food.getFoodValue();
+        System.out.println(catSatyety);
     }
+     public static Food getFood(){
+
+     }
 }
